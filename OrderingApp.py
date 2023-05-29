12bb10d5ba85a47fb39a0c9d82846355046d85b6
Marcus Loh <marcus.loh@my.bdsc.school.nz>
2023-05-30 11:57:48 +1200

    testing commit, added more purpose of code

--- OrderingApp.py
+++ OrderingApp.py
@@ -1,6 +1,8 @@
 from tkinter import *
 window = Tk()
 window.geometry("500x500") #this is the resolution of the window
-window.title("Ordering App")
+window.title("Ordering App") #This is the title of the application
+
+
 
 window.mainloop()
